2465c3656b5ad172388005d1420244f22da25c91
LT_Name <572413378@qq.com>
2022-09-16 21:45:51 +0800

    优化活塞判断

--- src/main/java/cn/smallaswater/land/lands/data/LandListenerPn.java
+++ src/main/java/cn/smallaswater/land/lands/data/LandListenerPn.java
@@ -22,22 +22,55 @@ public class LandListenerPn implements Listener {
 
     @EventHandler
     public void onBlockPiston(BlockPistonEvent event) {
-        Block block = event.getBlock();
-        LandData data;
+        Block blockPistonBase = event.getBlock();
         ArrayList<Position> checkPositions = new ArrayList<>();
-        checkPositions.add(block.add(3, 0, 0));
-        checkPositions.add(block.add(-3, 0, 0));
-        checkPositions.add(block.add(0, 3, 0));
-        checkPositions.add(block.add(0, -3, 0));
-        checkPositions.add(block.add(0, 0, 3));
-        checkPositions.add(block.add(0, 0, -3));
+        Block checkBlock = blockPistonBase;
+        int maxDistance = event.isExtending() ? 12 : 2;
+        switch (event.getDirection()) {
+            case UP:
+                do {
+                    checkBlock = checkBlock.up();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.y - checkBlock.y) < maxDistance);
+                break;
+            case DOWN:
+                do {
+                    checkBlock = checkBlock.down();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.y - checkBlock.y) < maxDistance);
+                break;
+            case NORTH:
+                do {
+                    checkBlock = checkBlock.north();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.z - checkBlock.z) < maxDistance);
+                break;
+            case SOUTH:
+                do {
+                    checkBlock = checkBlock.south();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.z - checkBlock.z) < maxDistance);
+                break;
+            case WEST:
+                do {
+                    checkBlock = checkBlock.west();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.x - checkBlock.x) < maxDistance);
+                break;
+            case EAST:
+                do {
+                    checkBlock = checkBlock.east();
+                    checkPositions.add(checkBlock);
+                } while (checkBlock.getId() != Block.AIR && Math.abs(blockPistonBase.x - checkBlock.x) < maxDistance);
+                break;
+        }
         checkPositions.addAll(event.getBlocks());
         checkPositions.addAll(event.getDestroyedBlocks());
         for (Position position : checkPositions) {
-            data = DataTool.getPlayerLandData(position);
+            LandData data = DataTool.getPlayerLandData(position);
             if (data != null) {
                 if (data.getLandOtherSet().isOpen(OtherLandSetting.RED_STONE_OUT)) {
-                    for (Player player : DataTool.getAroundPlayers(block, 5)) {
+                    for (Player player : DataTool.getAroundPlayers(blockPistonBase, 5)) {
                         player.sendMessage(LandModule.getModule().getConfig().getTitle() + LandModule.getModule().getLanguage().translateString("usingThePistonOutside", data.getLandName()));
                     }
                     event.setCancelled();
