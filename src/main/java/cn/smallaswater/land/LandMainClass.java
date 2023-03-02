f75ad3f611bcdd193b12d4ed5055f11f827f7dd5
LT_Name <572413378@qq.com>
2023-03-02 21:47:45 +0800

    启用资源包不再需要重启服务器

--- src/main/java/cn/smallaswater/land/LandMainClass.java
+++ src/main/java/cn/smallaswater/land/LandMainClass.java
@@ -2,11 +2,20 @@ package cn.smallaswater.land;
 
 import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.resourcepacks.ResourcePack;
+import cn.nukkit.resourcepacks.ResourcePackManager;
+import cn.nukkit.resourcepacks.ZippedResourcePack;
+import cn.smallaswater.land.lands.utils.LandConfig;
 import cn.smallaswater.land.module.LandModule;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import updata.AutoData;
 
 import java.io.File;
-import java.nio.file.Files;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 
 /**
@@ -22,6 +31,8 @@ public class LandMainClass extends PluginBase {
     @Override
     public void onLoad() {
         MAIN_CLASS = this;
+
+        this.saveResource("Resource/Land-ResourcePack.zip", true);
     }
 
     @Override
@@ -32,7 +43,7 @@ public class LandMainClass extends PluginBase {
             }
         }
 
-        File resourcePack = new File(Server.getInstance().getDataPath() + "resource_packs/Land-ResourcePack.zip");
+        /*File resourcePack = new File(Server.getInstance().getDataPath() + "resource_packs/Land-ResourcePack.zip");
         if (!resourcePack.exists()) {
             try {
                 Files.copy(this.getResource("Land-ResourcePack.zip"), resourcePack.toPath());
@@ -41,11 +52,35 @@ public class LandMainClass extends PluginBase {
             } catch (Exception e) {
                 this.getLogger().warning("Resource pack file replication failed! This may cause some functions to fail!", e);
             }
-        }
+        }*/
 
         module = new LandModule();
         module.moduleRegister();
 
+        if (LandConfig.getLandConfig().isEnableEnhancedResourcePack()) {
+            File file = new File(this.getDataFolder() + "/Resource/Land-ResourcePack.zip");
+            if (file.exists()) {
+                ResourcePackManager manager = this.getServer().getResourcePackManager();
+                synchronized (manager) {
+                    try {
+                        List<ResourcePack> packs = new ObjectArrayList<>();
+                        packs.add(new ZippedResourcePack(file));
+
+                        Field resourcePacksById = ResourcePackManager.class.getDeclaredField("resourcePacksById");
+                        resourcePacksById.setAccessible(true);
+                        Map<UUID, ResourcePack> byId = (Map<UUID, ResourcePack>) resourcePacksById.get(manager);
+                        packs.forEach(pack -> byId.put(pack.getPackId(), pack));
+
+                        Field resourcePacks = ResourcePackManager.class.getDeclaredField("resourcePacks");
+                        resourcePacks.setAccessible(true);
+                        packs.addAll(Arrays.asList((ResourcePack[]) resourcePacks.get(manager)));
+                        resourcePacks.set(manager, packs.toArray(new ResourcePack[0]));
+                    } catch (Exception e) {
+                        this.getLogger().error("Resource pack loading failed! This may cause some functions to fail!", e);
+                    }
+                }
+            }
+        }
     }
 
     public LandModule getModule() {
